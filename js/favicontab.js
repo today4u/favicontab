aa03804131308de1bcee5b5b85090c62674f33ab
today4u <today4u@users.noreply.github.com>
2018-02-02 23:40:01 +0900

    add favicontab page

---
+++ js/favicontab.js
@@ -0,0 +1,26 @@
+var board = document.getElementById("board");
+chrome.bookmarks.getChildren(localStorage['useFolderId'],function(roots){
+  roots.forEach(function (node){
+    (function outputBookmark(node) {
+      if(typeof node.url !== "undefined") {
+        var a   = document.createElement('a');
+        a.setAttribute('href', node.url);
+        a.setAttribute('id',   node.id);
+        a.setAttribute('index',node.index);
+        a.setAttribute('class','favicon');
+        var img = document.createElement('img');
+        img.setAttribute('src','chrome://favicon/'+node.url);
+        img.setAttribute('title',node.title);
+        a.appendChild(img);
+        board.appendChild(a);
+      } else {
+        chrome.bookmarks.getChildren(node.id, function(roots){
+          roots.forEach(function (node) {
+            outputBookmark(node);
+          });
+        });
+      }
+    }(node));
+  });
+});
+
---
+++ js/installed.js
@@ -0,0 +1,12 @@
+//拡張機能インストール時に実行
+chrome.runtime.onInstalled.addListener(function() {
+//localstorageにinstall時に新規作成したフォルダ情報があるかチェック
+  if(typeof localStorage.createdFolder === "undefined") {
+    //新規作成フォルダ情報が無い場合は、新規作成し,作成したfolderのidをlocalStorageに保存
+    chrome.bookmarks.create({"title": "FaviconTabFolder"},function(newFolder) {
+      localStorage['createdFolder'] = newFolder.id;
+      localStorage['useFolderId']   = newFolder.id;
+    });
+  }
+});
+
