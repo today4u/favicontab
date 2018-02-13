5d69ca15235ee056d1e69121fecf3745bfc39e77
today4u <today4u@users.noreply.github.com>
2018-02-13 22:59:02 +0900

    add: drag & drop favicon

--- js/favicontab.js
+++ js/favicontab.js
@@ -1,4 +1,5 @@
 var mode = 'link';
+var idPrefix = 'bid'
 document.body.style.backgroundColor = localStorage['backgroundColor'];
 var board = document.getElementById("board");
 chrome.bookmarks.getChildren(localStorage['useFolderId'],function(roots){
@@ -7,25 +8,48 @@ chrome.bookmarks.getChildren(localStorage['useFolderId'],function(roots){
       if(typeof node.url !== "undefined") {
         var img = document.createElement('img');
         img.setAttribute('src',      'chrome://favicon/'+node.url);
-        img.setAttribute('title',    node.title);
-        img.setAttribute('data-url', node.url);
-        img.setAttribute('id',       node.id);
-        img.setAttribute('index',    node.index);
+        img.setAttribute('title',      node.title);
+        img.setAttribute('id',         idPrefix+node.id);
+        img.setAttribute('data-url',   node.url);
+        img.setAttribute('data-index', node.index);
         img.setAttribute('class',    'favicon');
+        img.setAttribute('draggable','true');
         board.appendChild(img);
       } else {
-        chrome.bookmarks.getChildren(node.id, function(roots){
-          roots.forEach(function (node) {
-            outputBookmark(node);
-          });
-        });
+        //chrome.bookmarks.getChildren(node.id, function(roots){
+        //  roots.forEach(function (node) {
+        //    outputBookmark(node);
+        //  });
+        //});
       }
     }(node));
   });
 });
 
-var clickLink = function(ev) {
-    window.open(ev.target.dataset.url, '_blank');
+var link = function(ev) {
+  window.open(ev.target.dataset.url, '_blank');
 }
-board.addEventListener("click", clickLink);
 
+board.addEventListener("click", link, false);
+board.addEventListener('dragstart',function(event){
+  var el = document.getElementById(event.target.id);
+  el.classList.add('draging');
+},false);
+board.addEventListener('dragover',function(event){
+  event.preventDefault();
+  event.dataTransfer.dropEffect = 'move';
+},false);
+board.addEventListener('drop',   function(event){
+  var el = document.getElementsByClassName('draging');
+  var bookmarkId  = el[0].id.slice(idPrefix.length);
+  var changeIndex = Number(event.target.dataset.index);
+  if(event.target.dataset.index > el[0].dataset.index) {
+    changeIndex =  changeIndex+1;
+  }
+  chrome.bookmarks.move(bookmarkId, {"index": changeIndex});
+},false);
+board.addEventListener('dragend',function(event){
+  var el = document.getElementById(event.target.id);
+  console.log(el);
+  el.classList.remove("draging");
+},false);
