9e3e71512ec5c83e73b4be47660a08f1d2ba3c58
today4u <today4u@users.noreply.github.com>
2018-03-11 23:10:13 +0900

    clean: options

--- src/js/options.js
+++ src/js/options.js
@@ -1,49 +1,46 @@
 const jsonPath = '../json/options.json';
 
-//useFolder
-chrome.bookmarks.getTree(function(roots){
-  var folders  = [];  
-  roots.forEach(parser);
-  function parser(node){
-    if (node.children) {
-      if(node.id != "0") {
-        folders.push({"value":node.id, "index":node.index, "label":node.title, "parentId":node.parentId});
+const buildUseFolder = function() {
+  chrome.bookmarks.getTree(function(roots){
+    const folders  = [];  
+    roots.forEach(parser);
+    function parser(node){
+      if (node.children) {
+        if(node.id != "0") {
+          folders.push({"value":node.id, "index":node.index, "label":node.title, "parentId":node.parentId});
+        }
+        node.children.forEach(parser);
       }
-      node.children.forEach(parser);
+    }
+    buildOptionSettingRadio("useFolder",folders);
+  });
+  //json 
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if ((xhr.readyState === 4) && (xhr.status === 200)) {
+      const data = JSON.parse(xhr.responseText);
+      buildOptionSettingRadio("placement",  data.placement);
+      buildOptionSettingRadio("folderType", data.folderType);
+      buildOptionSettingRadio("linkTarget", data.linkTarget);
     }
   }
-  buildOptionSettingRadio("useFolder",folders);
-});
+  xhr.open("GET",jsonPath, true);
+  xhr.send(null);
 
-// options.json
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-  if ((xhr.readyState === 4) && (xhr.status === 200)) {
-    var data = JSON.parse(xhr.responseText);
-    buildOptionSettingRadio("placement",  data.placement);
-    buildOptionSettingRadio("folderType", data.folderType);
-    buildOptionSettingRadio("linkTarget", data.linkTarget);
-  }
+  //backgroundColor
+  const backgroundColor = document.getElementById('backgroundColor');
+  backgroundColor.setAttribute('value',localStorage['backgroundColor']);
+  backgroundColor.addEventListener("change", function (changeEvent) {
+    localStorage['backgroundColor'] = changeEvent.target.value;
+  });
 }
-xhr.open("GET",jsonPath, true);
-xhr.send(null);
-
-//backgroundColor
-var backgroundColor = document.getElementById('backgroundColor');
-backgroundColor.setAttribute('value',localStorage['backgroundColor']);
-backgroundColor.addEventListener("change", function (changeEvent) {
-  localStorage['backgroundColor'] = changeEvent.target.value;
-});
-
-
-
 
 function buildOptionSettingRadio (name, dataObject) { //
-  var el = document.getElementById(name);
+  const el = document.getElementById(name);
   dataObject.forEach(function(node) {
-    var label   = document.createElement('label');
-    var input   = document.createElement('input');
-    var text    = document.createTextNode(node.label);
+    const label   = document.createElement('label');
+    const input   = document.createElement('input');
+    const text    = document.createTextNode(node.label);
     input.setAttribute('value', node.value);
     input.setAttribute('type', 'radio');
     input.setAttribute('name', name);
@@ -51,7 +48,7 @@ function buildOptionSettingRadio (name, dataObject) { //
       input.setAttribute('id', node.id);
     }
     if(localStorage[name] == node.value) {
-        input.setAttribute('checked', 'checked');
+      input.setAttribute('checked', 'checked');
     }
     label.appendChild(input);
     label.appendChild(text);
@@ -62,23 +59,25 @@ function buildOptionSettingRadio (name, dataObject) { //
       return;
     }
     localStorage[name] = clickEvent.target.value;
-    valueControl(name, clickEvent.target.value);
+    setDisabled();
   });
 }
-const valueControl = function(name, value) {
-   switch(name) {
-      case 'placement':
-        const folderType = document.getElementById('folderType');
-        if(value === "1") {
-          localStorage.setItem('folderType',0);
-          document.getElementById('folderType0').checked = true;
-          folderType.setAttribute('disabled','disabled');
-        } else {
-          folderType.removeAttribute('disabled');
-        }
-        console.log(folderType.child);
-        break;
-      default:
-        break;
-   }
+
+const setDisabled = function() {
+  const folderType  = document.getElementById('folderType');
+  if(localStorage.getItem('placement') === "1") {
+    localStorage.setItem('folderType',0);
+    if(document.getElementById('folderType0')) {
+      document.getElementById('folderType0').checked = true;
+    }
+    folderType.setAttribute('disabled','disabled');
+  } else {
+    folderType.removeAttribute('disabled');
+  }
 }
+
+const promise = Promise.resolve();
+promise
+  .then(buildUseFolder())
+  .then(setDisabled());
+
